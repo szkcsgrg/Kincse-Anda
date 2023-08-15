e53931a652092035ee50152801e5f7e034ec4c9b
Gergő Szakács <szkcsgrg@gmail.com>
2023-08-15 14:04:31 +0200

    Delete db.php

--- src/components/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$DB_PATH = "mysql.rackhost.hu";
-$DB_NAME = "c35460anda";
-$DB_USERNAME = "c35460anda";
-$DB_PASSWORD = "Bloom2022";
-
-$conn = new mysqli($DB_PATH, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
-$conn->set_charset("UTF8");
